996a53990fe943dca0898a947c2feb8913ee6358
Val-d-emar <vhot@live.ru>
2024-02-12 13:51:43 +0400

    init: npm init npm install --save-dev @jest/globals @types/jest @types/node @types/uuid cross-env dotenv eslint jest nodemon prettier ts-jest ts-node ts-node-dev typescript uuid webpack webpack-cli npm install -S cross-env dotenv nodemon ts-node uuid webpack webpack-cli npx ts-jest config:init config tests

---
+++ src/index.test.ts
@@ -0,0 +1,12 @@
+// Uncomment the code below and write your tests
+import {str} from '.';
+
+describe('Scenario 1', () => {
+  test('test 1', async () => {
+    expect(str).toBe("Hello");
+  });
+
+  test('test 2', () => {
+    expect(str).toBe("Hello");
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+console.log("Hello World");
+export const str:string = 'Hello';
